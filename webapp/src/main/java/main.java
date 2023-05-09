ae46d9eb998c6479aeac29c7c16f88a237c86515
Joaquin <juaki00@gmail.com>
2023-05-09 11:33:04 +0200

    version estable

--- webapp/src/main/java/main.java
+++ webapp/src/main/java/main.java
@@ -1,16 +1,18 @@
+import edu.fpdual.web.fpdualweb.api.dto.Usuario;
 import edu.fpdual.web.fpdualweb.controller.UserController;
 import edu.fpdual.web.fpdualweb.manager.impl.UserManagerImpl;
 
 import java.sql.SQLException;
 
-public class main {
+public class    main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        UserController userController = new UserController(new UserManagerImpl());
-//        System.out.println(userController.findAllUsuario().size());
+        UserController userController = new UserController();
+//        Usuario user = new Usuario("nnnnnn","nuennnnnnnnvo","4321","b","123","a@a.com");
+//        System.out.println(userController.getAllUsers().size());
 //        System.out.println(userController.findUsuarioById("joaquin123").size());
-//        System.out.println(userController.findAllUsuario());
+//        System.out.println(userController.getAllUsers());
 //        System.out.println(userController.findUsuarioById("pablito123"));
-//        //userController.insertUser("nuevo","nuevo","4321","b","123","a@a.com");
+//          userController.insertUser(user);
 //        System.out.println(userController.passwordFromNick("nuevo"));
 //        System.out.println(userController.existeNick("joaquin123"));
 //        System.out.println(userController.existeNick("joaquin321"));
